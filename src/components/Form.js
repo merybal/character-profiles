153ebba2d446304046978bf92665ba49fe533710
Maria Baltar <merybaltar@gmail.com>
2023-03-07 12:14:32 -0300

    implemented single state in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Form.css";
 
 const Form = () => {
+  // const [enteredFirstName, setEnteredFirstName] = useState("");
+  // const [enteredLastName, setEnteredLastName] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    enteredFirstName: "",
+    enteredLastName: "",
+    enteredAge: "",
+  });
   const fistNameChangeHandler = (event) => {
-    console.log(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    setUserInput((previousState) => {
+      return { ...previousState, enteredFirstName: event.target.value };
+    });
+  };
+
+  const lastNameChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredLastName: event.target.value,
+    // });
+    setUserInput((previousState) => {
+      return { ...previousState, enteredLastName: event.target.value };
+    });
+  };
+
+  const ageChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAge: event.target.value,
+    // });
+    setUserInput((previousState) => {
+      return { ...previousState, enteredAge: event.target.value };
+    });
   };
 
   return (
@@ -15,11 +50,11 @@ const Form = () => {
       </div>
       <div>
         <label>Last Name: </label>
-        <input type="text" />
+        <input type="text" onChange={lastNameChangeHandler} />
       </div>
       <div>
         <label>Age: </label>
-        <input type="number" />
+        <input type="number" onChange={ageChangeHandler} />
       </div>
       <div>
         <button type="submit">Add Character</button>
